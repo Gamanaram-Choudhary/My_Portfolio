f4823f4f2a2657e12e0485edb7c50141ad6cfe2b
Gamanaram Choudhary <harjeeramchoudhary604@gmail.com>
2024-01-09 02:16:30 +0530

    A responsive header and section is ready.

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// var typed = new typed(".text", {
+//   strings: ["Fronted Devloper", "YouTuber", "Web Developer"],
+//   typeSpeed: 100,
+//   backSpeed: 100,
+//   backDelay: 1000,
+//   loop: true,
+// });
+
+var typed = new Typed(".text", {
+  strings: ["Web Developer","Programmer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+// var typed = new typed(".text", {
+//   Strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
+//   typeSpeed: 50,
+// });
